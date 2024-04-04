2b9cf4e2001d455fcd448b17dc46f16841adfa8c
karma098 <kumarhritihik804@gmail.com>
2024-04-04 13:04:00 +0530

    authentication and middleware

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express=require('express');
 const app=express();
 const db=require('./db');
 require('dotenv').config();
+const passport=require('./auth');
 
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());
+const PORT=process.env.PORT || 3000;
 
+//Middleware function
+const logRequest=(req,res,next)=>{
+  console.log(`(${new Date().toLocaleString()}) Request made to: ${req.originalUrl}`);
+  next();
+}
+
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localAuthMiddleware=passport.authenticate('local',{session:false});
 
 app.get('/',function(req,res){
   res.send('Welcome to my hotel...How can i help you?,, we have list of menus');
@@ -17,9 +29,7 @@ app.use('/menu',menuRoutes);
 
 
 const personRoutes=require('./routes/personRoutes');
-app.use('/person',personRoutes);
-
-const PORT=process.env.PORT || 3000;
+app.use('/person',localAuthMiddleware,personRoutes);
 
 app.listen(PORT,()=>{
   console.log('listening on port 3000');
